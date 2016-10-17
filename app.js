df068fa5c0a4eaba81b316b4cca2a3ac699066ba
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-10-16 20:38:19 -0400

    Implement post request endpoint

--- app.js
+++ app.js
@@ -9,4 +9,16 @@ app.get('/breakfast', (req, res) => {
   res.send('<h1>Breakfast</h1>' + Menu.getBreakfast())
 })
 
+app.post('/', (req, res) => {
+  const meal = req.query.meal
+  const name = req.query.name
+  const price = req.query.price
+  const sale = req.query.sale
+
+  if (meal === 'breakfast') {
+    Menu.addBreakfast(name, price, sale)
+  }
+  res.sendStatus(200)
+})
+
 app.listen(3000, () => console.log('Listening on Port 3000'))
