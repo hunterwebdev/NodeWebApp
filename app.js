d5fc5b8a446e6d6c5046b7104f9b51371fc108ad
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-10-16 20:36:01 -0400

    Add body-parser to allow POST requests

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import Menu from './Menu.js'
 
 let app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/breakfast', (req, res) => {
   res.send('<h1>Breakfast</h1>' + Menu.getBreakfast())
